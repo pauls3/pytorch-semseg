69cf2d80cba90df2a8eb8ce28bf1e96cf521a1a9
pauls3 <paulstanik3@gmail.com>
2022-01-27 11:25:18 -0800

    commented out print statements for data paths

--- ptsemseg/loader/cityscapes_loader.py
+++ ptsemseg/loader/cityscapes_loader.py
@@ -161,14 +161,14 @@ class cityscapesLoader(data.Dataset):
         img = np.array(img, dtype=np.uint8)
 
         
-        print(lbl_path)
-        print("---")
-        print(self.annotations_base)
-        print("---")
-        print(img_path.split(os.sep)[-2])
-        print("---")
-        print(os.path.basename(img_path)[:-15] + "gtFine_labelIds.png")
-        print(" ")
+        # print(lbl_path)
+        # print("---")
+        # print(self.annotations_base)
+        # print("---")
+        # print(img_path.split(os.sep)[-2])
+        # print("---")
+        # print(os.path.basename(img_path)[:-15] + "gtFine_labelIds.png")
+        # print(" ")
 
         lbl = plt.imread(lbl_path)
         lbl = self.encode_segmap(np.array(lbl, dtype=np.uint8))
