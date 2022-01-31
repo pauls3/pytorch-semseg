95d914a783671e0ea8d250721ea8513126261742
pauls3 <paulstanik3@gmail.com>
2022-01-30 17:35:59 -0800

    modified data loader to fix error

--- validate_callable.py
+++ validate_callable.py
@@ -43,15 +43,22 @@ def validate(cfg, args):
     # if val_delta < 1.0:
     #     val_delta = -1.0
         
+    # loader = data_loader(
+    #     data_path,
+    #     split=cfg["data"]["val_split"],
+    #     is_transform=True,
+    #     img_size=(cfg["model"].get("input_size",[cfg["data"].get("img_rows","same"), "same"])[0] , cfg["model"].get("input_size",["same",cfg["data"].get("img_cols", "same")])[1]),
+    #     version=cfg["data"].get("version","cityscapes"),
+    #     # asp_ratio_delta_min = 1.0/val_delta,
+    #     # asp_ratio_delta_max = val_delta,
+    #     img_norm=cfg["data"].get("img_norm",True),
+    # )
+
     loader = data_loader(
         data_path,
         split=cfg["data"]["val_split"],
         is_transform=True,
-        img_size=(cfg["model"].get("input_size",[cfg["data"].get("img_rows","same"), "same"])[0] , cfg["model"].get("input_size",["same",cfg["data"].get("img_cols", "same")])[1]),
-        version=cfg["data"].get("version","cityscapes"),
-        # asp_ratio_delta_min = 1.0/val_delta,
-        # asp_ratio_delta_max = val_delta,
-        img_norm=cfg["data"].get("img_norm",True),
+        img_size=(cfg["data"]["img_rows"], cfg["data"]["img_cols"]),
     )
 
     n_classes = loader.n_classes
