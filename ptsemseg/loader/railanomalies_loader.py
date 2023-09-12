93c8ceb943b686c436a578504e36c25181465dfe
pauls3 <paulstanik3@gmail.com>
2023-09-11 21:07:09 -0700

    checking shape of rail anomalies dataset

--- ptsemseg/loader/railanomalies_loader.py
+++ ptsemseg/loader/railanomalies_loader.py
@@ -161,6 +161,7 @@ class railanomaliesLoader(data.Dataset):
         :param lbl:
         """
         img = m.imresize(img, (self.img_size[0], self.img_size[1]))  # uint8 with RGB mode
+        print(img.shape)
         img = img[:, :, ::-1]  # RGB -> BGR
         img = img.astype(np.float64)
         img -= self.mean
