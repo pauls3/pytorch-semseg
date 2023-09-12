1891cd8c23ea4c6d4106332e502f121d19b1cda9
pauls3 <paulstanik3@gmail.com>
2023-09-11 20:07:08 -0700

    checking mean of img

--- ptsemseg/loader/railanomalies_loader.py
+++ ptsemseg/loader/railanomalies_loader.py
@@ -160,9 +160,7 @@ class railanomaliesLoader(data.Dataset):
         :param img:
         :param lbl:
         """
-        print(self.img_size[0])
-        print(self.img_size[1])
-
+        print(self.mean)
         img = m.imresize(img, (self.img_size[0], self.img_size[1]))  # uint8 with RGB mode
         img = img[:, :, ::-1]  # RGB -> BGR
         img = img.astype(np.float64)
